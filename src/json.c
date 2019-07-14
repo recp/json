4fd20101daca02e2435dae9931bdee55d51eea52
Recep Aslantas <info@recp.me>
2019-07-14 13:59:26 +0300

    Update json.c

--- src/json.c
+++ src/json.c
@@ -27,7 +27,7 @@ json_key(const char ** __restrict ptr, int * __restrict keysize) {
 
   start = end = pi;
 
-  while (c != ':' && c != '\"' && c != '\'') {
+  while (c != ':' && c != '\"' && c != '\'' && c != '\0') {
     if (c != ' ')
       end = pi + 1;
     
@@ -62,7 +62,8 @@ json_value(const char ** __restrict ptr, int * restrict valuesize) {
          && c != '{'
          && c != '}'
          && c != '['
-         && c != ']') {
+         && c != ']'
+         && c != '\0') {
     if (c != ' ')
       end = pi + 1;
     
@@ -76,41 +77,28 @@ json_value(const char ** __restrict ptr, int * restrict valuesize) {
   return start;
 }
 
-json_t*
+void
 json_child(json_doc_t  * __restrict doc,
-           json_t      * __restrict parent,
-           bool                     isvalue) {
-  json_t     *root, *child;
+           json_t      * __restrict parent) {
+  json_t     *obj;
   const char *key;
   int         keysize;
   char        c;
+  bool        lookingForKey;
 
   if (!doc->ptr || (c = *doc->ptr) == '\0')
-    return NULL;
+    return;
 
-  root = NULL;
+  key            = NULL;
+  obj            = NULL;
+  keysize        = 0;
+  lookingForKey  = false;
 
   do {
-  cont:
-
-    /* skip white spaces */
-    do {
-      switch (c) {
-        case ' ':
-        case '\t':
-        case '\n':
-        case '\r':
-        case '\0':
-          c = *++doc->ptr;
-          continue;
-        default:
-          goto chr;
-      }
-    } while (true);
-
-  chr:
+  again:
     /* child */
     switch (c) {
+<<<<<<< Updated upstream
       case '{': {
         json_t *object, *child;
         object       = json_calloc(doc, sizeof(json_t));
@@ -124,22 +112,42 @@ json_child(json_doc_t  * __restrict doc,
         }  else {
           object->type = JSON_OBJECT;
          }
+=======
+      /* trim */
+      case ' ':
+      case '\r':
+      case '\n':
+      case '\t':
+        c = *++doc->ptr;
+        continue;
+      case '{':
+      case '[': {
+        obj = json_calloc(doc, sizeof(json_t));
 
-        ++doc->ptr;
-        if ((child = json_child(doc, object, false))) {
-          child->prev   = parent;
-          child->next   = object->child;
-          object->child = child;
+        if (c == '{') {
+          obj->type     = JSON_OBJECT;
+          lookingForKey = true;
+        } else {
+          obj->type = JSON_ARRAY;
+        }
+>>>>>>> Stashed changes
+
+        /* parent must not be NULL */
+        obj->prev     = parent;
+        obj->next     = parent->child;
+        parent->child = obj;
+
+        if (key) {
+          obj->key     = key;
+          obj->keySize = keysize;
+          key          = NULL;
         }
 
-        continue;
-      }
-      case '[': {
-        json_t *object;
-        object = json_calloc(doc, sizeof(json_array_t));
+        c = *++doc->ptr;
         break;
       }
       case '}':
+<<<<<<< Updated upstream
         goto ret;
     }
 
@@ -183,31 +191,100 @@ json_child(json_doc_t  * __restrict doc,
           continue;
         default:
           goto val;
+=======
+      case ']':
+        c   = *++doc->ptr;
+        obj = NULL;
+        break;
+      case ',': {
+        lookingForKey = true;
+>>>>>>> Stashed changes
       }
-    } while (true);
+      default: {
+        /* looking for key */
+        if (lookingForKey) {
+          key = json_key(&doc->ptr, &keysize);
+          if (key == NULL || ((c = *key) == '\0'))
+            goto err;
+          lookingForKey = false;
+          
+          /* jump to value */
+          do {
+            switch (c) {
+              case ' ':
+              case '\t':
+              case '\n':
+              case '\r':
+              case '"':
+              case '\'':
+              case ':':
+                c = *++doc->ptr;
+                continue;
+              default:
+                goto val;
+            }
+          } while (true);
+          
+        val:
+          goto again;
+        }
 
+<<<<<<< Updated upstream
   val:
     if ((child->child = json_child(doc, child, true))) {
       // if (child->type == JSON_UNKOWN)
         // child->type = JSON_OBJECT;
+=======
+        /* looking for value */
+        else {
+          printf("looking for value\n");
+          json_t *val;
+          val       = json_calloc(doc, sizeof(json_t));
+          val->type = JSON_STRING;
+
+          /* parent must not be NULL */
+          val->prev  = obj;
+          val->next  = obj->child;
+          obj->child = val;
+
+          if (key) {
+            val->key     = key;
+            val->keySize = keysize;
+            key          = NULL;
+          }
+          
+          val->value = json_value(&doc->ptr, &val->valSize);
+          c = *doc->ptr;
+          goto again;
+
+        } /* if lookingForKey */
+      } /* switch->default */
+>>>>>>> Stashed changes
     }
   } while ((c = *doc->ptr) != '\0' && (c = *++doc->ptr) != '\0');
 
+err:
 ret:
-  return root;
+  return ;
 }
 
 json_t*
 json_parse(const char * __restrict contents) {
   json_doc_t *doc;
-  json_t     *root;
+  json_t      tmproot;
 
-  doc          = calloc(1, sizeof(*doc));
-  doc->memroot = calloc(1, JSON_MEM_PAGE);
-  doc->ptr     = contents;
-  root         = json_child(doc, NULL, false);
+  doc           = calloc(1, sizeof(*doc));
+  doc->memroot  = calloc(1, JSON_MEM_PAGE);
+  doc->ptr      = contents;
+  tmproot.child = NULL;
 
-  return root;
+  json_child(doc, &tmproot);
+
+  if (tmproot.child) {
+    tmproot.child->prev = NULL;
+  }
+
+  return tmproot.child;
 }
 
 json_t*
