2f02622d7655f835e26d7b52e6eb6ce328785d25
Recep Aslantas <info@recp.me>
2019-07-14 14:43:19 +0300

    improve print json

--- include/print.h
+++ include/print.h
@@ -12,35 +12,56 @@
 #include <stdlib.h>
 
 void
-print_json(json_t *json) {
+print_json_pad(json_t *json, int pad) {
   char *key, *value;
-  
+  int   i;
+
   while (json) {
-    printf("\t");
+    for (i = 0; i < pad; i++)
+      printf("\t");
     switch (json->type) {
       case JSON_OBJECT:
-        printf("{\n\t");
-        print_json(json->child);
+        printf("{\n");
+        print_json_pad(json->child, pad + 1);
+
+        for (i = 0; i < pad; i++)
+          printf("\t");
         printf("}");
+
+        if (json->next)
+          printf(",");
+
+        printf("\n");
         break;
-        
+
       case JSON_STRING:
         key   = alloca(json->keySize);
         value = alloca(json->valSize);
-        
+
         snprintf(key,   json->keySize + 1, "%s", json->key);
         snprintf(value, json->valSize + 1, "%s", json->value);
-        
-        printf("\"%s\": \"%s\"\n", key, value);
-        break;
-      default:
+
+        printf("\"%s\": \"%s\"", key, value);
+
+        if (json->next)
+          printf(",");
         printf("\n");
         break;
+      default:
+        break;
     }
-    printf("\t");
-    
+
+
+
     json = json->next;
   }
+}
+
+void
+print_json(json_t *json) {
+  printf("json ( %p ):\n", json);
+  print_json_pad(json, 0);
+  printf("\n");
 }
 
 #endif /* json_print_h */
