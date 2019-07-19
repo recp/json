944cf79fc2700e942040987323aa767482f3dde3
Recep Aslantas <info@recp.me>
2019-07-19 22:42:43 +0300

    util func to get null-terminated string
    * add documentation for util functions

--- include/json/util.h
+++ include/json/util.h
@@ -12,6 +12,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*!
+ * @brief creates number (int32) from string value
+ *
+ * @param[in] object       json object
+ * @param[in] defaultValue default value if operation fails
+ * @return number
+ */
 JSON_INLINE
 int32_t
 json_int32(const json_t * __restrict object, int32_t defaultValue) {
@@ -21,6 +28,13 @@ json_int32(const json_t * __restrict object, int32_t defaultValue) {
   return (int32_t)strtol(object->value, NULL, 10);
 }
 
+/*!
+ * @brief creates number (uint32) from string value
+ *
+ * @param[in] object       json object
+ * @param[in] defaultValue default value if operation fails
+ * @return number
+ */
 JSON_INLINE
 uint32_t
 json_uint32(const json_t * __restrict object, uint32_t defaultValue) {
@@ -30,6 +44,13 @@ json_uint32(const json_t * __restrict object, uint32_t defaultValue) {
   return (uint32_t)strtoul(object->value, NULL, 10);
 }
 
+/*!
+ * @brief creates number (int64) from string value
+ *
+ * @param[in] object       json object
+ * @param[in] defaultValue default value if operation fails
+ * @return number
+ */
 JSON_INLINE
 int64_t
 json_int64(const json_t * __restrict object, int64_t defaultValue) {
@@ -39,6 +60,13 @@ json_int64(const json_t * __restrict object, int64_t defaultValue) {
   return strtoll(object->value, NULL, 10);
 }
 
+/*!
+ * @brief creates number (uint64) from string value
+ *
+ * @param[in] object       json object
+ * @param[in] defaultValue default value if operation fails
+ * @return number
+ */
 JSON_INLINE
 uint64_t
 json_uint64(const json_t * __restrict object, uint64_t defaultValue) {
@@ -48,6 +76,13 @@ json_uint64(const json_t * __restrict object, uint64_t defaultValue) {
   return strtoull(object->value, NULL, 10);
 }
 
+/*!
+ * @brief creates number (float) from string value
+ *
+ * @param[in] object       json object
+ * @param[in] defaultValue default value if operation fails
+ * @return number
+ */
 JSON_INLINE
 float
 json_float(const json_t * __restrict object, float defaultValue) {
@@ -57,6 +92,13 @@ json_float(const json_t * __restrict object, float defaultValue) {
   return strtof(object->value, NULL);
 }
 
+/*!
+ * @brief creates number (double) from string value
+ *
+ * @param[in] object       json object
+ * @param[in] defaultValue default value if operation fails
+ * @return number
+ */
 JSON_INLINE
 double
 json_double(const json_t * __restrict object, double defaultValue) {
@@ -66,12 +108,41 @@ json_double(const json_t * __restrict object, double defaultValue) {
   return strtod(object->value, NULL);
 }
 
+/*!
+ * @brief return non-NULL terminated string value
+ *
+ * you must use object->valSize to copy, compare ... string
+ *
+ * @param[in] object json object
+ * @return non-NULL terminated string value (pointer only)
+ */
 JSON_INLINE
 const char*
 json_string(const json_t * __restrict object) {
   return (const char *)object->value;
 }
 
+/*!
+ * @brief return NULL terminated duplicated string value
+ *
+ * value must be free-ed when no longer needed
+ *
+ * @param[in] object json object
+ * @return NULL terminated duplicated string value
+ */
+JSON_INLINE
+const char*
+json_string_dup(const json_t * __restrict object) {
+  return strndup(object->value, object->valSize);
+}
+
+/*!
+ * @brief compares json key with a string
+ *
+ * @param[in] obj json object
+ * @param[in] str string to compare
+ * @return true if str is equals to json's key
+ */
 JSON_INLINE
 bool
 json_key_eq(const json_t * __restrict obj, const char * __restrict str) {
